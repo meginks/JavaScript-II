1aefbe5ef02643331a3f92bfda1d1139d88c6902
unknown <38929577+meginks@users.noreply.github.com>
2019-01-22 20:26:52 -0600

    I think I got the answer to #2 on closure.js now

--- assignments/closure.js
+++ assignments/closure.js
@@ -30,23 +30,24 @@ console.log(president);
 // ==== Challenge 2: Create a counter function ====
 const counter = () => { 
 
+// Return a function that when invoked increments and returns a counter variable.
+
 let count = 0;
 
 function countUp() {
-  return count += 1;
+  count++;
+  return count;
 }
 
-countUp();
+return countUp();
+
 }; 
-  // Return a function that when invoked increments and returns a counter variable.
-// }; 
 
-let newCounter = counter();
-
-console.log(newCounter);
+const newCounter = counter();
 
 
-/// I'm really confused by this one.
+console.log(newCounter); 
+
 
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
